593b81ea8773dcb9a9231c30c8a2c00f37da04b5
Anjali Tyagi <anjalityagi988@gmail.com>
2020-12-09 15:29:23 -0500

    Fixed the errors while defining the object inside of the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 let pokemonList= [
-    pikachu {
+    {
         name: 'Pikachu',
         heightInMetres: 2,
         type: 'grass',
     },
-    bulbasaur {
+    {
         name: 'bulbasaur',
         heightInMetres: 3,
         type: 'fire',
     },
-    chikadex {
+    {
         name: 'chikadex',
         heightInMetres: 7,
         type: 'water',
