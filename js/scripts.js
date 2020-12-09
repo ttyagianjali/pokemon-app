87191cdac32fb06f2cf30271366e4645e6098dea
Anjali Tyagi <anjalityagi988@gmail.com>
2020-12-08 20:56:30 -0500

    Practicing Arrays and objects

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList= [
+    pikachu {
+        name: 'Pikachu',
+        heightInMetres: 2,
+        type: 'grass',
+    },
+    bulbasaur {
+        name: 'bulbasaur',
+        heightInMetres: 3,
+        type: 'fire',
+    },
+    chikadex {
+        name: 'chikadex',
+        heightInMetres: 7,
+        type: 'water',
+    }
+];
