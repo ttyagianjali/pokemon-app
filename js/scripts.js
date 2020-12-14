f25ec61b108603dcdf259b896f0377c59e9152a1
ttyagianjali <74582328+ttyagianjali@users.noreply.github.com>
2020-12-14 02:20:57 -0500

    Today I made for loop using conditionals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
+//declaring array over multiple objects
 let pokemonList= [
     {
         name: 'Pikachu',
-        heightInMetres: 2,
+        height: 2,
         type: 'grass',
     },
     {
-        name: 'bulbasaur',
-        heightInMetres: 3,
+        name: 'Bulbasaur',
+        height: 3,
         type: 'fire',
     },
     {
-        name: 'chikadex',
-        heightInMetres: 7,
+        name: 'Chikadex',
+        height: 7,
         type: 'water',
     }
 ];
+  //this piece of code is when I just want to print the names of the pokemons on the document with their heights
+  // for( let i=0; i<pokemonList.length; i++){
+       // if(i<=2){
+       //  document.write('My name is  ' + pokemonList[i].name + ' and I am '  + pokemonList[i].height + ' metres tall. ' 
+      //  }
+  // }
+
+
+  //this piece of code is with conditional which checks the heights of the pokemons and prints "wow thst's big" on the side of the tallest pokemon's name.
+   
+   for( let i=0; i<pokemonList.length; i++){
+       if(pokemonList[i].height>0 && pokemonList[i].height<5){
+        document.write('My name is  ' + pokemonList[i].name + ' and I am '  + pokemonList[i].height + ' metres tall. '); 
+       }
+       
+       else{
+        document.write('My name is  ' + pokemonList[i].name + ' and I am '  + pokemonList[i].height + ' metres tall. ' + " *Wow that's big!");
+    }
+}
